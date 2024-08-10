75b6ee37266ee38209ac279a11bcd5080aca320c
v <example>
2024-08-10 18:55:28 +0600

    Add ability to choose games

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const games = [
-    { name: 'Bike', appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50', promoId: '43e35910-c168-4634-ad4f-52fd764a843f' },
-    { name: 'Cube', appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2', promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3' },
-    { name: 'Clone', appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb', promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767' },
-    { name: 'Train', appToken: '82647f43-3f87-402d-88dd-09a90025313f', promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954' }
+    {name: 'Bike', appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50', promoId: '43e35910-c168-4634-ad4f-52fd764a843f'},
+    {name: 'Cube', appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2', promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3'},
+    {name: 'Clone', appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb', promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767'},
+    {name: 'Train', appToken: '82647f43-3f87-402d-88dd-09a90025313f', promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'}
 ];
 
 let ready_codes = [];
+let codesCount = {
+    bike: 0,
+    cube: 0,
+    clone: 0,
+    train: 0
+};
+
+const MAX_CODES_PER_GAME = 4;
+const MIN_CODES = 1;
 
 async function generateClientId() {
     const timestamp = Date.now();
-    const randomNumbers = Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join('');
+    const randomNumbers = Array.from({length: 19}, () => Math.floor(Math.random() * 10)).join('');
     return `${timestamp}-${randomNumbers}`;
 }
 
@@ -21,7 +30,7 @@ async function loginClient(appToken) {
             clientId: clientId,
             clientOrigin: 'deviceid'
         }, {
-            headers: { 'Content-Type': 'application/json; charset=utf-8' }
+            headers: {'Content-Type': 'application/json; charset=utf-8'}
         });
         return response.data.clientToken;
     } catch (error) {
@@ -79,7 +88,7 @@ async function createCode(token, promoId) {
 }
 
 function generateRandomUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0,
             v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
@@ -95,36 +104,96 @@ function copyToClipboard(text) {
 }
 
 async function generateCodes() {
-    document.getElementById('keygenButton').setAttribute('disabled', '');
-    const output = document.getElementById('output');
-    output.innerHTML = 'Generating codes... Click on the code to copy';
+    const keygenButton = document.getElementById('keygenButton');
+    keygenButton.setAttribute('disabled', '');
+    const output = document.getElementById('output-codes');
+    output.innerHTML = ''
+    const output_text = document.getElementById('generating-text');
+    const finishMessage = document.getElementById('generated-text');
+    output_text.classList.remove('hidden')
+    finishMessage.classList.add('hidden')
 
     ready_codes = [];
-    for (const game of games) {
-        for (let i = 0; i < 4; i++) {
-            try {
-                const token = await loginClient(game.appToken);
-                await registerEvent(token, game.promoId);
-                const code = await createCode(token, game.promoId);
-                ready_codes.push({ game: game.name, code });
+    codesCount = {bike: 0, cube: 0, clone: 0, train: 0};
 
-                // Add the generated code to the output with a click handler
-                const codeElement = document.createElement('p');
-                codeElement.textContent = `${code}`;
-                codeElement.addEventListener('click', () => copyToClipboard(code));
-                output.appendChild(codeElement);
-            } catch (error) {
-                const errorElement = document.createElement('p');
-                errorElement.textContent = `${game.name} Code ${i + 1}: Error generating code`;
-                output.appendChild(errorElement);
+    const selectedGames = Array.from(document.querySelectorAll('.game-button.active')).map(button => button.getAttribute('data-game'));
+    const totalCodesPerGame = parseInt(document.getElementById('codeCount').textContent);
+
+    for (const game of games) {
+        if (selectedGames.includes(game.name.toLowerCase())) {
+            while (codesCount[game.name.toLowerCase()] < totalCodesPerGame) {
+                try {
+                    const token = await loginClient(game.appToken);
+                    await registerEvent(token, game.promoId);
+                    const code = await createCode(token, game.promoId);
+                    ready_codes.push({game: game.name, code});
+                    codesCount[game.name.toLowerCase()]++;
+
+                    // Add the generated code to the output with a click handler
+                    const codeElement = document.createElement('p');
+                    codeElement.textContent = `${code}`;
+                    codeElement.addEventListener('click', () => copyToClipboard(code));
+                    output.appendChild(codeElement);
+
+                    // Break out of the loop if we have reached the limit
+                    if (codesCount[game.name.toLowerCase()] >= totalCodesPerGame) {
+                        break;
+                    }
+                } catch (error) {
+                    const errorElement = document.createElement('p');
+                    errorElement.textContent = `${game.name} Code Error`;
+                    output.appendChild(errorElement);
+                }
             }
         }
     }
 
-    const finishMessage = document.createElement('p');
-    finishMessage.textContent = 'All codes generated!';
-    output.appendChild(finishMessage);
+    output_text.classList.add('hidden')
+    finishMessage.classList.remove('hidden')
+
+    keygenButton.removeAttribute('disabled');
+}
+
+const gameButtons = document.querySelectorAll('.game-button');
+
+gameButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active');
+        updateGameSelection();
+    });
+});
+
+const decreaseButton = document.getElementById('decreaseCount');
+const increaseButton = document.getElementById('increaseCount');
+const codeCountSpan = document.getElementById('codeCount');
+let codeCount = parseInt(codeCountSpan.textContent, 10);
+
+decreaseButton.addEventListener('click', () => {
+    if (codeCount > MIN_CODES) {
+        codeCount--;
+        codeCountSpan.textContent = codeCount;
+        updateButtonStates();
+    }
+});
+
+increaseButton.addEventListener('click', () => {
+    if (codeCount < MAX_CODES_PER_GAME) {
+        codeCount++;
+        codeCountSpan.textContent = codeCount;
+        updateButtonStates();
+    }
+});
+
+function updateButtonStates() {
+    decreaseButton.disabled = (codeCount <= MIN_CODES);
+    increaseButton.disabled = (codeCount >= MAX_CODES_PER_GAME);
+}
+
+function updateGameSelection() {
+    const selectedGames = Array.from(document.querySelectorAll('.game-button.active'))
+        .map(button => button.getAttribute('data-game'));
+    console.log('Selected games:', selectedGames);
+    // Логика для обработки выбранных игр
 }
 
 document.getElementById('keygenButton').addEventListener('click', generateCodes);
-
