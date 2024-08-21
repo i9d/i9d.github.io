ab0362d66fdb2d6bd6c438ec59e9818db645dd38
v <example>
2024-08-21 14:19:32 +0600

    Fix polysphere game tokens

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const games = [
     {name: 'Train', appToken: '82647f43-3f87-402d-88dd-09a90025313f', promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'},
     {name: 'Merge', appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833', promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4'},
     {name: 'Twerk', appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c', promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'},
-    {name: 'Polysphere', appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"', promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"'},
+    {name: 'Polysphere', appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71'},
 ];
 
 let ready_codes = [];
