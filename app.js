1d6ada6dd43d55948818a1b8787e90401c5fdb91
v <example>
2024-09-19 00:08:14 +0600

    Add new games

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const games = [
     {name: 'stone', appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af'},
     {name: 'bounce', appToken: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', promoId: 'bc72d3b9-8e91-4884-9c33-f72482f0db37'},
     {name: 'ball', appToken: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600', promoId: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600'},
+    {name: 'count', appToken: '4bdc17da-2601-449b-948e-f8c7bd376553', promoId: '4bdc17da-2601-449b-948e-f8c7bd376553'},
+    {name: 'pinout', appToken: 'd2378baf-d617-417a-9d99-d685824335f0', promoId: 'd2378baf-d617-417a-9d99-d685824335f0'},
 ];
 
 let ready_codes = [];
